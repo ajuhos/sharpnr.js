419a1fcbf60521ec12fd31137804cb8380e19832
Ádám Juhos <info@sharpnrjs.com>
2012-07-03 18:11:19 +0200

    HOTFIX

--- dev/await.js/await.language.js
+++ dev/await.js/await.language.js
@@ -4,7 +4,7 @@ var await = {};
 
 //This method of the library enables you to define async methods in JavaScript.
 function async(method) {
-  return sharpnr.await.compiler.compile(method);
+  return sharpnr.await.compiler.compile(method.toString());
 }
 
 //Define foreach to avoid errors.
